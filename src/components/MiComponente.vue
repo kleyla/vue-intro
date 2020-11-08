8f333d7b9938307e7004353470afa69bdc3f5738
kleyla <kleyla01@live.com>
2020-11-07 22:38:37 -0400

    component changes

--- src/components/MiComponente.vue
+++ src/components/MiComponente.vue
@@ -2,6 +2,10 @@
   <div id="mi-componente">
     <h1>Este es mi componente</h1>
     <h2>{{ subtitulo }}</h2>
+    <p>{{ greeting() }}</p>
+    <ul>
+      <li v-for="(girl, index) in girls" v-bind:key="index">{{ girl }}</li>
+    </ul>
   </div>
 </template>
 
@@ -11,7 +15,18 @@ export default {
   data() {
     return {
       subtitulo: "Soy el subtitulo",
+      girls: ["Karen", "yol", "andy"],
     };
+  },
+  methods: {
+    greeting: function() {
+      return "hi!";
+    },
   },
 };
 </script>
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
